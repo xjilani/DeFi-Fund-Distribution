b3b794ffa053cc7a1bf959a941217d8209d84b5b
xjilani <xjilani@gmail.com>
2024-11-05 19:42:40 +0530

    ERC721 Creation

---
+++ ERC721.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract MyToken is ERC721,ERC721URIStorage, Ownable {
+    mapping(uint256=>bytes) tokenData;
+    constructor() ERC721("VritaVentures", "VritaNFT")  Ownable(msg.sender) {}
+    function mintNFT(address to_, uint256 tokenId_,string memory uri_,bytes memory data_) public payable   {
+        _safeMint(to_, tokenId_,data_);
+        _setTokenURI(tokenId_,uri_);
+        tokenData[tokenId_]=data_;
+    }
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+    function getData(uint256 tokenId_) public view returns(bytes memory){
+        /*
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+        if (bytes(_tokenURI).length > 0) {
+            return string.concat(base, _tokenURI);
+        }
+        */
+        return(tokenData[tokenId_]);
+    }
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+
+}
